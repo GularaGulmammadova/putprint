27d25273538c6720ea43398224ebca0dd06d0307
gülare gülmemmedova <gularagulmammadova@gmail.com>
2024-10-16 23:57:13 +0400

    putandprint

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 import Nav from './Components/Nav/Nav'
 import Products from './Components/Products/Products'
 import ProductDetail from './Components/ProductDetail/ProductDetail'
+import About from './Components/About/About'
 import { Route, Routes } from 'react-router'
+
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path='/products' element={<Products/>} />
           <Route path='/productdetail' element={<ProductDetail/>}/>
+          <Route path='/about' element={<About/>} />
         </Routes>
     </div>
   )
---
+++ src/Components/backgroundImage/backgroundImage.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './backgroundImage.css'
+
+const backgroundImage = () => {
+  return (
+    <div className="background-image">
+    <div className="content">
+      <h2 className="designer-heading">Dizaynıvıza başlamağa hazırsınız?</h2>
+      <button className="start-now-design">İndi Başla</button>
+    </div>
+  </div>
+  )
+}
+
+export default backgroundImage
