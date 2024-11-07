f5aadae7e16104234259f4b95763d575e4d540d6
Diana Baghirova <diana.baghirova18@gmail.com>
2024-11-07 21:08:39 +0400

    pricesAdded

--- src/Components/ManageFiles/ManageFiles.jsx
+++ src/Components/ManageFiles/ManageFiles.jsx
@@ -2,8 +2,9 @@
 import styles from './ManageFiles.module.css'
 import ColorPicker from '../ColorPicker/ColorPicker'
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
 
-const ManageFiles = ({setColor, productName}) => {
+const ManageFiles = ({setColor, product}) => {
 
   const [size, setSize] = useState('S');
   const [material, setMaterial] = useState('Nazik');
@@ -14,38 +15,39 @@ const ManageFiles = ({setColor, productName}) => {
   return (
     <div className={styles.file}>
       <div className={styles.btns}>
-        <button className={styles.btn}>Məhsullar</button>
+        <Link to='/products' className={styles.btn}>Məhsullar</Link>
         <button className={`${styles.btn} ${styles.chosen}`}>Özün yarat</button>
       </div>
 
       <div className={styles.picker}>
         <h2 className={styles.heading}>Oversize T-shirt</h2>
         <hr className={styles.hr} />
-        {productName!=="Eko Çanta" && <>
+        {product.name!=="Eko Çanta" && <>
           <h3 className={styles.title}>Məhsul rəngi</h3>  
           <ColorPicker setColor={setColor} />
         </>}
 
-        <h3 className={styles.title}>Ölçü</h3>   
+        {(product.name!=='Kepka' && product.name!=='Eko Çanta') && <><h3 className={styles.title}>Ölçü</h3>   
         <div className={styles.btns}>
           {sizes.map((s, i) => <button className={size===s ? `${styles.chosen} ${styles.roundBtn}` : styles.roundBtn} style={{backgroundColor: `${s}`}} key={i} onClick={() => setSize(s)}>{s}</button>)}
-        </div>
+        </div></>}
         
-        <h3 className={styles.title}>Material</h3>   
+        {(product.name === "Sviter" || product.name === "Kapşonlu Sviter") && <><h3 className={styles.title}>Material</h3>   
         <div className={styles.btns}>
           {materials.map((m, i) => <button className={material===m ? `${styles.chosen} ${styles.roundBtn} ${styles.long}` : `${styles.roundBtn} ${styles.long}`} key={i} onClick={() => setMaterial(m)}>{m}</button>)}
-        </div>
+        </div></>}
 
         <div className={styles.spaceBetween}>
-          <h3 className={styles.title}>Ümumi Qiymət</h3>
-          <h3 className={styles.title}>25 AZN</h3>
+          <h3 className={styles.title}>Ümümi Qiymət</h3>
+          {!product.minPrice && <h3 className={styles.title}>{product.price}</h3>}
+          {product.minPrice && <h3 className={styles.title}>{material==='Nazik' ? product.minPrice : product.maxPrice}</h3> }
         </div> 
         
         <button className={styles.orderBtn}>Sifariş et</button>
       </div>
 
       <div className={styles.warning}>
-        <h3>{`Çərçivədən kənarda qalan dizaynlar çap olunmayacaq!`}</h3>
+        <h3>{`! Don't go out of the borders`}</h3>
       </div>
     </div>
   )
